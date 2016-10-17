6ae3b9c8e5c9dcfa46bc8d6ca128eb0011862fe2
yinte12 <yinte112@163.com>
2016-10-17 18:07:02 +0800

    add lock

--- app/src/main/java/com/asuper/aidldemo/User.java
+++ app/src/main/java/com/asuper/aidldemo/User.java
@@ -1,9 +1,13 @@
 package com.asuper.aidldemo;
 
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 /**
  * Created by Super on 2016/9/8.
  */
 public class User {
+    private Queue<String> queue = new ArrayBlockingQueue<String>(100);
     private String name;
     private Integer order;
     public String getName() {
