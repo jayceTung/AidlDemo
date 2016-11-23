5712ff118c8a6dc4d86a2bbf7391c7d270eb705d
yinte12 <yinte112@163.com>
2016-11-23 18:01:49 +0800

    add tree

---
+++ app/src/main/java/com/asuper/aidldemo/collection/TreeNode.java
@@ -0,0 +1,15 @@
+package com.asuper.aidldemo.collection;
+
+/**
+ * Created by Super on 2016/11/23.
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
