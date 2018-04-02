1604bd11b98a1deb887b855706dadfefc5ffe86b
Super <minchao.dong@melot.cn>
2018-04-02 11:27:02 +0800

    feature: gson默认值

--- app/src/main/java/com/asuper/aidldemo/java/EnumTest.java
+++ app/src/main/java/com/asuper/aidldemo/java/EnumTest.java
@@ -25,7 +25,7 @@ public enum EnumTest {
                 + "" + EnumTest.FILE_NOT_EXIST.getDeclaringClass());
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("isodd  =" + isOdd(i));
+            System.out.println("isodd =" + isOdd(i));
         }
         float i = 10000;
         int j = 33;
@@ -34,79 +34,8 @@ public enum EnumTest {
         String mDefent = String.format("%.2f", percent);
         System.out.println("isodd  =" + mDefent);
 
-
-//        String str = "{\n" +
-//                "    \"msgTag\": 10030203,\n" +
-//                "    \"time\": 10,\n" +
-//                "    \"round\": 1,\n" +
-//                "    \"total\": 12,\n" +
-//                "    \"topicId\": 1,\n" +
-//                "    \"topicContent\": \"题目\",\n" +
-//                "    \"options\": [\n" +
-//                "        {\n" +
-//                "            \"optionId\": 1,\n" +
-//                "            \"optionContent\": \"题目\"\n" +
-//                "        },\n" +
-//                "        {\n" +
-//                "            \"optionId\": 2,\n" +
-//                "            \"optionContent\": \"题目\"\n" +
-//                "        },\n" +
-//                "        {\n" +
-//                "            \"optionId\": 3,\n" +
-//                "            \"optionContent\": \"题目\"\n" +
-//                "        },\n" +
-//                "        {\n" +
-//                "            \"optionId\": 4,\n" +
-//                "            \"optionContent\": \"题目\"\n" +
-//                "        }\n" +
-//                "    ]\n" +
-//                "}";
-//        Question que = GsonUtil.GsonToBean(str, Question.class);
-//
-//        mLogicControl = new AnswerLogicControl(AnswerRoom.this, mPopRoot);
-//
-//        mLogicControl.onStartAnswer(que);
-
-
-
-//        String str = "{\n" +
-//                "    \"MsgTag\": 10030204,\n" +
-//                "    \"isRevive\": false,\n" +
-//                "    \"selectOptionId\": 3,\n" +
-//                "    \"correctId\": 2,\n" +
-//                "    \"round\": 1,\n" +
-//                "    \"total\": 12,\n" +
-//                "    \"topicId\": 1,\n" +
-//                "    \"topicContent\": \"题目\",\n" +
-//                "    \"analysis\": \"blabla\",\n" +
-//                "    \"options\": [\n" +
-//                "        {\n" +
-//                "            \"optionId\": 1,\n" +
-//                "            \"optionContent\": \"选项A\",\n" +
-//                "            \"selectCount\": 300\n" +
-//                "        },\n" +
-//                "        {\n" +
-//                "            \"optionId\": 1,\n" +
-//                "            \"optionContent\": \"选项A\",\n" +
-//                "            \"selectCount\": 300\n" +
-//                "        },\n" +
-//                "        {\n" +
-//                "            \"optionId\": 1,\n" +
-//                "            \"optionContent\": \"选项A\",\n" +
-//                "            \"selectCount\": 300\n" +
-//                "        },\n" +
-//                "        {\n" +
-//                "            \"optionId\": 1,\n" +
-//                "            \"optionContent\": \"选项A\",\n" +
-//                "            \"selectCount\": 300\n" +
-//                "        }\n" +
-//                "    ]\n" +
-//                "}";
-//        Question que = GsonUtil.GsonToBean(str, Question.class);
-//
-//        mLogicControl = new AnswerLogicControl(AnswerRoom.this, mPopRoot);
-//
-//        mLogicControl.onResultShow(que, false, 1, 1);
+        int a = 3 << 2;
+        System.out.println("num = " + a);
     }
 
     public static boolean isOdd(int a) {
