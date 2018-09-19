beda6591dc23583cb7fa45987858461daf3516b9
Super <yinte112@163.com>
2018-09-19 19:49:34 +0800

    feature: add 通用下拉刷新组件

---
+++ app/src/main/java/com/asuper/aidldemo/view/refresh/PullToRefreshListener.java
@@ -0,0 +1,22 @@
+package com.asuper.aidldemo.view.refresh;
+
+/**
+ * @author super
+ * @date 2018/9/19
+ */
+public interface PullToRefreshListener {
+    /**
+     * 刷新中
+     * @param refreshLayout
+     */
+    void onRefresh(RefreshLayout refreshLayout);
+
+    /**
+     * 开始刷新
+     */
+    void onStartRefresh();
+    /**
+     * 刷新完成
+     */
+    void onFinishRefresh();
+}
