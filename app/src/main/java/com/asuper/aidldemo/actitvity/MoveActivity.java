3281bdf7348553f8853157dbecbb2aee2503b31c
Super <yinte112@163.com>
2020-02-26 14:30:51 +0800

    feat: add search history

--- app/src/main/java/com/asuper/aidldemo/actitvity/MoveActivity.java
+++ app/src/main/java/com/asuper/aidldemo/actitvity/MoveActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.asuper.aidldemo.R;
+import com.asuper.aidldemo.view.SearchHistView;
 
 /**
  * @author super
@@ -16,6 +19,9 @@ import com.asuper.aidldemo.R;
  */
 public class MoveActivity extends AppCompatActivity {
     private View view1, view2, view3, view4;
+    private SearchHistView mShvView;
+
+    public static String[] data = {"净水器", "手机", "电动车", "洗衣机", "沙发", "冰箱", "瓷砖", "空调", "床垫", "卫浴", "热水器", "床", "家具", "手表", "电视", "集成灶", "领带", "保温杯", "童装", "自行车", "空气净化器", "地板", "硅藻泥", "油烟机", "智能家居"};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,6 +36,34 @@ public class MoveActivity extends AppCompatActivity {
         view2.setOnClickListener(v -> moveToTarget(view2, view4));
         view3.setOnClickListener(v -> moveToTarget(view3, view4));
 
+        mShvView = (SearchHistView) findViewById(R.id.shv_view);
+        setFlow();
+    }
+
+    public void setFlow(){
+//        noSearch.setVisibility(View.GONE);
+        for (int i = 0; i < data.length; i++) {
+            final TextView textView = (TextView) LayoutInflater.from(this).inflate(R.layout.item_tv, mShvView, false);
+            textView.setText(data[i]);
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //自定义点击事件，这里我写出点击item执行删除操作
+//                    for (int j = 0; j < data.length; j++) {
+//                        if (data[i].equals(textView.getText().toString())) {
+//                            data.remove(j);
+//                            if (data.size() == 0){
+//                                noSearch.setVisibility(View.VISIBLE);
+//                            }
+//                            flowView.removeView(v);
+//                            StorageUtil.remove(SearchActivity.this,String.valueOf(j),false);
+//                            break;
+//                        }
+//                    }
+                }
+            });
+            mShvView.addView(textView);
+        }
     }
 
     private void moveToTarget(View startView, View endView) {
